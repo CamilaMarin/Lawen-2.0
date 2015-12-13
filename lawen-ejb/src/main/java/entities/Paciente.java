806143d8fd7666de6274e39dad7b570596fdddff
Francisco I. Avello B <francisco.avello@usach.cl>
2015-12-13 14:06:54 -0300

    Revert "Añadido agregar niño."
    This reverts commit 1f21ae0fe59d9982de8cdf304fa041aae46b81fe.

--- lawen-ejb/src/main/java/entities/Paciente.java
+++ lawen-ejb/src/main/java/entities/Paciente.java
@@ -12,7 +12,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
